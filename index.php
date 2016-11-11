f2eecb9c1516c8c1f725d0a6cb606f613f36898c
Varun <sriram.varun672@gmail.com>
2016-11-11 14:40:40 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+require_once('TwitterAPIExchange.php');
+/** Set access tokens here - see: https://dev.twitter.com/apps/ **/
+$settings = array(
+'oauth_access_token' => "118661137-CFs4Mf2YGl461w0MXTZmUsAXbiAxVHS2G6IxL1uk",
+'oauth_access_token_secret' => "1oLhUApuw1SZibvFkdpyRn1bO7RmUDTLJjWIKO8Ryg0vq",
+'consumer_key' => "aKSaUZ4SS4PbomJx0Wun40wDl",
+'consumer_secret' => "hCEZU0p9IGwS47D3h2zjjsNOMwab8TjJHlgEFP2P8kRNqs1rh7"
+);
+$url = "https://api.twitter.com/1.1/statuses/user_timeline.json";
+$requestMethod = "GET";
+if (isset($_GET['user'])) {$user = $_GET['user'];} else {$user = "sriramvarun";}
+if (isset($_GET['count'])) {$count = $_GET['count'];} else {$count = 20;}
+$getfield = "?screen_name=$user&count=$count";
+
+
+
+$twitter = new TwitterAPIExchange($settings);
+
+$test = file_get_contents("https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=iagdotme&count=50");
+
+echo $test;
+
+$string = json_decode($twitter->setGetfield($getfield)
+->buildOauth($url, $requestMethod)
+->performRequest(),$assoc = TRUE);
+if($string["errors"][0]["message"] != "") {echo "<h3>Sorry, there was a problem.</h3><p>Twitter returned the following error message:</p><p><em>".$string[errors][0]["message"]."</em></p>";exit();}
+foreach($string as $items)
+    {
+        echo "Time and Date of Tweet: ".$items['created_at']."<br />";
+        echo "Tweet: ". $items['text']."<br />";
+        echo "Tweeted by: ". $items['user']['name']."<br />";
+        echo "Screen name: ". $items['user']['screen_name']."<br />";
+        echo "Followers: ". $items['user']['followers_count']."<br />";
+        echo "Friends: ". $items['user']['friends_count']."<br />";
+        echo "Listed: ". $items['user']['listed_count']."<br /><hr />";
+    }
+?>
